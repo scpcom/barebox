9cee9b59e6d0c3f96f29e878579e987536c29af8
scpcom <scpcom@gmx.de>
2024-07-13 22:07:41 +0200

    cleanup build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ mkdir -p BUILD_OUT
 
 make distclean
 make sequoia_defconfig
-[ "X$CROSS_COMPILE" = "Xarm-linux-gnueabihf-" ] && sed -i s/'# CONFIG_AEABI is not set'/'CONFIG_AEABI=y'/g .config
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- MEM_SIZE=1G
+[ "X$CROSS_COMPILE" = "Xarm-linux-gnueabihf-" ] && sed -i s/'^# CONFIG_AEABI is not set'/'CONFIG_AEABI=y'/g .config
+make
 cp barebox.bin BUILD_OUT/
 
 make distclean
 make sequoia-uloader_defconfig
-[ "X$CROSS_COMPILE" = "Xarm-linux-gnueabihf-" ] && sed -i s/'# CONFIG_AEABI is not set'/'CONFIG_AEABI=y'/g .config
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- MEM_SIZE=1G
+[ "X$CROSS_COMPILE" = "Xarm-linux-gnueabihf-" ] && sed -i s/'^# CONFIG_AEABI is not set'/'CONFIG_AEABI=y'/g .config
+make
 cp uloader.bin BUILD_OUT/
 
 cd BUILD_OUT
